e09e81e93082986e8dd444d7ee211e13da6f71d0
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-08 23:37:53 +0400

    Created UncontrolledOnOff.tsx component

---
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -0,0 +1,62 @@
+import { FC, useState } from 'react'
+
+type PropsType = {
+  onChange: (on: boolean) => void
+}
+export const UncontrolledOnOff: FC<PropsType> = ({ onChange }) => {
+  console.log('UncontrolledOnOff rendering')
+
+  const [on, setOn] = useState<boolean>(false)
+
+  const onStyle = {
+    width: '30px',
+    height: '20px',
+    border: '1px solid black',
+    display: 'inline-block',
+    padding: '2px',
+    backgroundColor: on ? 'green' : 'white',
+  }
+  const offStyle = {
+    width: '30px',
+    height: '20px',
+    border: '1px solid black',
+    display: 'inline-block',
+    marginLeft: '2px',
+    padding: '2px',
+    backgroundColor: on ? 'white' : 'red',
+  }
+  const indicatorStyle = {
+    width: '10px',
+    height: '10px',
+    borderRadius: '5px',
+    border: '1px solid black',
+    display: 'inline-block',
+    marginLeft: '5px',
+    backgroundColor: on ? 'green' : 'red',
+  }
+
+  const onClicked = () => {
+    if (!on) {
+      setOn(!on)
+      onChange(!on)
+    }
+  }
+  const offClicked = () => {
+    if (on) {
+      setOn(!on)
+      onChange(!on)
+    }
+  }
+
+  return (
+    <div>
+      <div style={onStyle} onClick={onClicked}>
+        On
+      </div>
+      <div style={offStyle} onClick={offClicked}>
+        Off
+      </div>
+      <div style={indicatorStyle}></div>
+    </div>
+  )
+}

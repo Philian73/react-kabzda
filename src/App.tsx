8aff86fdcf380b461116369d73810c6c1bf47c90
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-08 23:35:13 +0400

    Created VITE project REACT + TS SWC

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import './styles/App.css'
+
+import { Accordion } from './components/Accordion/Accordion.tsx'
+import { Rating, RatingValueType } from './components/Rating/Rating.tsx'
+import { UncontrolledAccordion } from './components/UncontrolledAccordion/UncontrolledAccordion.tsx'
+import { UncontrolledOnOff } from './components/UncontrolledOnOff/UncontrolledOnOff.tsx'
+import { UncontrolledRating } from './components/UncontrolledRating/UncontrolledRating.tsx'
+
+const App = () => {
+  console.log('App rendering')
+
+  const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+  const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+  const [switchOn, setSwitchOn] = useState<boolean>(false)
+
+  return (
+    <div className="App">
+      <Rating value={ratingValue} onClick={setRatingValue} />
+      <UncontrolledRating />
+      <Accordion
+        titleValue="Menu"
+        collapsed={accordionCollapsed}
+        onChange={() => setAccordionCollapsed(!accordionCollapsed)}
+      />
+      <UncontrolledAccordion>Menu 2</UncontrolledAccordion>
+      {/*<OnOff on={switchOn} onChange={setSwitchOn} />*/}
+      <UncontrolledOnOff onChange={setSwitchOn} /> {switchOn.toString()}
+    </div>
+  )
+}
+
+export default App

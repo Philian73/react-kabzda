15c2321f72b9564993279732031ce7325b72f5d6
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-08 23:37:44 +0400

    Created UncontrolledAccordion.tsx component

---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,39 @@
+import { FC, useState } from 'react'
+
+type PropsType = {
+  children: string
+}
+export const UncontrolledAccordion: FC<PropsType> = ({ children }) => {
+  console.log('UncontrolledAccordion rendering')
+
+  const [collapsed, setCollapsed] = useState<boolean>(false)
+
+  return (
+    <div>
+      <AccordionTitle title={children} onClick={() => setCollapsed(!collapsed)} />
+      {!collapsed && <AccordionBody />}
+    </div>
+  )
+}
+
+type AccordionTitlePropsType = {
+  title: string
+  onClick: () => void
+}
+const AccordionTitle: FC<AccordionTitlePropsType> = ({ title, onClick }) => {
+  console.log('AccordionTitle rendering')
+
+  return <h3 onClick={onClick}>{title}</h3>
+}
+
+const AccordionBody = () => {
+  console.log('AccordionBody rendering')
+
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}

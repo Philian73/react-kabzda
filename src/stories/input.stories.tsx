1b4e8a5d45d0923994866e4140d70524ab7152e4
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-10 03:01:24 +0400

    Added controlled input/text, input/checkbox, input/select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -62,3 +62,46 @@ export const ControlledInputWithFixedValue: Story = {
   },
   render: args => <input type="text" {...args} />,
 }
+
+export const ControlledInput: Story = {
+  render: () => {
+    const [value, setValue] = useState('')
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.currentTarget.value)
+    }
+
+    return <input type="text" value={value} onChange={onChangeHandler} />
+  },
+}
+
+export const ControlledCheckbox: Story = {
+  render: () => {
+    const [value, setValue] = useState(true)
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.currentTarget.checked)
+    }
+
+    return <input type="checkbox" checked={value} onChange={onChangeHandler} />
+  },
+}
+
+export const ControlledSelect: Story = {
+  render: () => {
+    const [value, setValue] = useState<string | undefined>('2')
+
+    const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+      setValue(event.currentTarget.value)
+    }
+
+    return (
+      <select value={value} onChange={onChangeHandler}>
+        <option>none</option>
+        <option value="1">Ulyanovsk</option>
+        <option value="2">Moscow</option>
+        <option value="3">Saint Petersburg</option>
+      </select>
+    )
+  },
+}

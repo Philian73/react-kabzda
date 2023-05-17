82b5c4708a7afdab94bd32bc34898895e144f2fc
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-17 20:37:16 +0400

    added local usestate

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -5,58 +5,57 @@ import { ItemType } from '../../App.tsx'
 import s from './Select.module.css'
 
 type PropsType = {
-  value?: any
-  onChange: (value: any) => void
+  value: any
   items: ItemType[]
 }
-export const Select: FC<PropsType> = memo(({ value, onChange, items }) => {
+export const Select: FC<PropsType> = memo(({ value, items }) => {
   console.log('Select rendering')
 
   const [active, setActive] = useState(false)
   const [hoveredItemValue, setHoveredItemValue] = useState(value)
 
   useEffect(() => {
-    setHoveredItemValue(value)
-  }, [value])
+    setHoveredItemValue(hoveredItemValue)
+  }, [hoveredItemValue])
 
   const toggleItems = () => setActive(!active)
 
   const onKeyDownHandler = (e: KeyboardEvent<HTMLDivElement>) => {
     if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
       for (let i = 0; i < items.length; i++) {
-        if (items[i].value === hoveredItemValue) {
+        if (items[i].id === hoveredItemValue) {
           const challengerElement = e.key === 'ArrowDown' ? items[i + 1] : items[i - 1]
 
-          challengerElement && onChange(challengerElement.value)
+          challengerElement && setHoveredItemValue(challengerElement.id)
 
           return
         }
       }
-      !selectedItem && onChange(items[0].value)
+      !selectedItem && setHoveredItemValue(items[0].id)
     }
     if (e.key === 'Enter' || e.key === 'Escape') {
       setActive(false)
     }
   }
 
-  const selectedItem = items.find(item => item.value === value)
-  const hoveredItem = items.find(item => item.value === hoveredItemValue)
+  const selectedItem = items.find(item => item.id === hoveredItemValue)
+  const hoveredItem = items.find(item => item.id === hoveredItemValue)
 
   const itemsMap = items.map(item => {
     const onClickHandler = () => {
-      onChange(item.value)
+      setHoveredItemValue(item.id)
       toggleItems()
     }
-    const onMouseEnterHandler = () => setHoveredItemValue(item.value)
+    const onMouseEnterHandler = () => setHoveredItemValue(item.id)
 
     return (
       <div
-        key={item.value}
+        key={item.id}
         className={`${s.item} ${hoveredItem === item ? s.selected : ''}`}
         onClick={onClickHandler}
         onMouseEnter={onMouseEnterHandler}
       >
-        {item.title}
+        {item.city}
       </div>
     )
   })
@@ -70,7 +69,7 @@ export const Select: FC<PropsType> = memo(({ value, onChange, items }) => {
         onKeyDown={onKeyDownHandler}
       >
         <span className={s.main} onClick={toggleItems}>
-          {selectedItem?.title}
+          {selectedItem?.city}
         </span>
         {active && <div className={s.items}>{itemsMap}</div>}
       </div>

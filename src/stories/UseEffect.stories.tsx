3f9c85ebb967f809ed6683774bfbd7b7afd22370
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-31 14:56:03 +0400

    added useEffect test stories

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,55 @@
+import { FC, memo, useEffect, useState } from 'react'
+
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Counter> = {
+  title: 'test/useEffect demo',
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+type CounterPropsType = {
+  count: number
+}
+const Counter: FC<CounterPropsType> = memo(({ count }) => {
+  return <div>{count}</div>
+})
+
+export const SimpleExample: Story = {
+  render: () => {
+    console.log('SimpleExample')
+
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    useEffect(() => {
+      console.log('useEffect every render')
+
+      document.title = counter.toString()
+    })
+    useEffect(() => {
+      console.log('useEffect only first render (componentDidMount)')
+
+      document.title = counter.toString()
+    }, [])
+    useEffect(() => {
+      console.log('useEffect first render and every counter change')
+
+      document.title = counter.toString()
+    }, [counter])
+
+    const incrementFake = () => setFake(fake + 1)
+    const incrementCounter = () => setCounter(counter + 1)
+
+    return (
+      <>
+        <button onClick={incrementFake}>fake+</button>
+        <Counter count={fake} />
+        <button onClick={incrementCounter}>counter+</button>
+        <Counter count={counter} />
+      </>
+    )
+  },
+}
